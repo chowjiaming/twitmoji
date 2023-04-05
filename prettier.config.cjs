6e864b3ab30d509714f9932b339302d28aa99dbe
Joseph Chow <contact@josephchow.dev>
2023-04-05 00:19:50 -0400

    chore: updated prisma schema client and document formatting :technologist:

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,10 @@
 /** @type {import("prettier").Config} */
 const config = {
+  singleQuote: true,
+  trailingComma: "es5",
+  arrowParens: "always",
+  endOfLine: "auto",
+  bracketSpacing: false,
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
 };
 
