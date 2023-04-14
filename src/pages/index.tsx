a9b6d03054d29088e6307a3e9984543aa3bcabaf
Joseph Chow <contact@josephchow.dev>
2023-04-14 15:20:30 -0400

    refactor: post button loading state and key press :recycle:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ import {type RouterOutputs, api} from '@/utils/api';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import Image from 'next/image';
-import {LoadingPage} from '@/components/loading';
+import {LoadingPage, LoadingSpinner} from '@/components/loading';
 import {toast} from 'react-hot-toast';
 
 dayjs.extend(relativeTime);
@@ -57,15 +57,29 @@ const CreatePostWizard = () => {
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            e.preventDefault();
+            if (input !== '') mutate({content: input});
+          }
+        }}
         disabled={isPosting}
       />
-      <button
-        onClick={() => {
-          mutate({content: input});
-        }}
-      >
-        Post
-      </button>
+      {input !== '' && !isPosting && (
+        <button
+          disabled={isPosting || input === ''}
+          onClick={() => {
+            mutate({content: input});
+          }}
+        >
+          Post
+        </button>
+      )}
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   );
 };
