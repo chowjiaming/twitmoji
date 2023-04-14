08afedcfb2f191ee27f964dc5d5845e521ddad1a
Joseph Chow <contact@josephchow.dev>
2023-04-14 15:31:22 -0400

    refactor: added profile and posts page :sparkles:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import Image from 'next/image';
 import {LoadingPage, LoadingSpinner} from '@/components/loading';
 import {toast} from 'react-hot-toast';
+import Link from 'next/link';
 
 dayjs.extend(relativeTime);
 
@@ -98,10 +99,16 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <span className="">{`@${author.username}`}</span>
-          <span>{`· ${dayjs(post.createdAt).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span className="">{`@${author.username}`}</span>
+          </Link>
+          <Link href={`/post/${post.id}}`}>
+            <span>{`· ${dayjs(post.createdAt).fromNow()}`}</span>
+          </Link>
         </div>
-        <span className="text-2xl">{post.content}</span>
+        <Link href={`/post/${post.id}}`}>
+          <span className="text-2xl">{post.content}</span>
+        </Link>
       </div>
     </div>
   );
