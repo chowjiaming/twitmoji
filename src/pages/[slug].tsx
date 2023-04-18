68fe6012bfbc4ede0745cf4401f5bb9bcc1f9d4b
Joseph Chow <contact@josephchow.dev>
2023-04-18 14:20:24 -0400

    refactor: profile page styling :art:

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -6,6 +6,7 @@ import {PageLayout} from '@/components/layout';
 import {api} from '@/utils/api';
 import {type GetStaticProps, type NextPage} from 'next';
 import Head from 'next/head';
+import Image from 'next/image';
 
 const ProfilePage: NextPage<{username: string}> = ({username}) => {
   const {data} = api.profile.getUserByUsername.useQuery({
@@ -24,7 +25,18 @@ const ProfilePage: NextPage<{username: string}> = ({username}) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <PageLayout>
-        <div>{data.username}</div>
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`${data.username ?? ''} profile pic`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black"
+          />
+        </div>
+        <div className="h-[64px]" />
+        <div className="p-4 text-2xl font-bold">{`@${data.username}`}</div>
+        <div className="w-full border-b border-slate-400" />
       </PageLayout>
     </>
   );
